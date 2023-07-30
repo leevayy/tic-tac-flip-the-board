6fc3ac24c031ab12461a96f1a9b4f8cf9333fe14
leevay <maoaoa7@gmail.com>
2023-07-30 12:02:11 +0700

    setted up the board for game

--- src/App.js
+++ src/App.js
@@ -1,3 +1,54 @@
-export default function Square() {
-  return <button className="square">X</button>;
+import React, { useState } from "react";
+
+export default function Game() {
+
+
+	return <Board />
+}
+
+function Board() {
+	const width = 3;
+	const height = 3;
+
+    const [board, setBoard] = useState(() => {
+        const defaultSquareValue = '';
+        const initialBoard = [];
+		// filling up 2d array with id's for each cell
+        for (let y = 0; y < height; y++) {
+            const row = [];
+            for (let x = 0; x < width; x++) {
+                row.push(defaultSquareValue);
+            }
+            initialBoard.push(row);
+        }
+        return initialBoard;
+    });
+
+	function changeSquareValue(square) {
+		// to identify cell we need to do some math.
+		// since each id=y*width+x
+		// y=(id-x)//width and x=id%width
+		let id = square.target.id;
+		let x = id%width;
+		let y = Math.floor( (id - x) / width )
+		
+		if (board[y][x] === '') {
+			const newBoard = [...board];
+			newBoard[y][x] = 'X'
+			setBoard( newBoard );
+		}
+	}
+
+	return board.map((boardRow, y) => (
+		<div className="board-row">
+			{boardRow.map( (val, x) => {
+				let id = y * width + x;
+				return <Square id={id} value={val} handleClick={changeSquareValue}></Square>
+			})}
+		</div>
+	));
+}
+
+function Square({id, value, handleClick}) {
+	return <button className="square" onClick={handleClick} id={id}>{value}</button>;
 }
